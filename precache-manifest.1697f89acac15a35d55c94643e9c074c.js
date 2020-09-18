9d487e1412a6bb22e5f83b3e0421f531d1dbd83c
Artemy-And <andruschak.artemy@gmail.com>
2020-09-18 12:42:53 +0300

    Updates

---
+++ precache-manifest.1697f89acac15a35d55c94643e9c074c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ac4c2ee3aa59dc6400ed04331afda53",
+    "url": "/todolist-git/index.html"
+  },
+  {
+    "revision": "53f00b1a19d99021b1a4",
+    "url": "/todolist-git/static/css/main.84802909.chunk.css"
+  },
+  {
+    "revision": "b30733e56662f60a1b61",
+    "url": "/todolist-git/static/js/2.44d990a2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todolist-git/static/js/2.44d990a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53f00b1a19d99021b1a4",
+    "url": "/todolist-git/static/js/main.8eb22c49.chunk.js"
+  },
+  {
+    "revision": "cc1ffc2b7e3f6230ce95",
+    "url": "/todolist-git/static/js/runtime-main.7fc638b0.js"
+  }
+]);
